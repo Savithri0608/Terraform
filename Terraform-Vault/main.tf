f32f50936b8cd05c9af52c5d5351b05c61389819
Savithri Marati <savithri62001@gmail.com>
2025-05-27 19:38:24 +0530

    Update main.tf

--- Terraform-Vault/main.tf
+++ Terraform-Vault/main.tf
@@ -1,1 +1,38 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 3.20.0"
+    }
+  }
+}
 
+
+provider "vault" {
+  address = "http://<>:8200"
+  skip_child_token = true
+
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id = "<>"
+      secret_id = "<>"
+    }
+  }
+}
+
+data "vault_kv_secret_v2" "example" {
+  mount = "kv" 
+  name  = "test-secret" 
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami id"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test"
+    Secret = data.vault_kv_secret_v2.example.data["username"]
+  }
+}
